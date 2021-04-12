696d7733f832dbb544f3fd82dc0b6c787fc184f6
Mehedi Hassan Shovo <41648862+mehedi1256@users.noreply.github.com>
2021-04-12 14:51:11 +0600

    Add files via upload

---
+++ WELabExam(id-181-15-1821sec-pc-a)/Q1/crudoperationDB-php/config.php
@@ -0,0 +1,9 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'crud_web3';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+?>
